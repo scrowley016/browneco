929b9ef52e1922f427d78f3b3bea427a938476ca
scrowley016 <33128271+scrowley016@users.noreply.github.com>
2021-02-07 21:24:58 -0500

    deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import App from './app'
-import {Provider} from "react-redux"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDom.render(
